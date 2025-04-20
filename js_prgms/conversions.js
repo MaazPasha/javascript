b179d5848f5a52675c11c65ad0e195a1d1fdbe72
MaazPasha <109347935+MaazPasha@users.noreply.github.com>
2025-04-20 20:51:44 +0000

    Data type conversion

---
+++ js_prgms/conversions.js
@@ -0,0 +1,17 @@
+let score = Object()
+console.log(typeof score);
+
+let value = String(score);
+console.log(typeof value)
+console.log(value);
+
+console.log("2"+2+2); //type coercion
+console.log(2+3%2+"1");
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
